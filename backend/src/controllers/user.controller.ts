a5aa942d9aa29e84b2b755dbf9508074a5e1fa48
Ankur Parchani <ankurparchani@gmail.com>
2024-01-02 16:19:28 +0530

    backend-add: passwordUpdate userUpdate requests

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import bcrypt from "bcrypt";
 import { Request, Response } from "express";
 import { User } from "../../global";
 import { db } from "../db/dbConnect";
@@ -35,6 +36,76 @@ export const getUser = async (req: CustomRequest, res: Response) => {
     });
   } catch (err) {
     res.status(500).send("Error from delete Account, check console");
+    console.log(err);
+  }
+};
+
+// Updating the user
+export const updateMe = async (req: CustomRequest, res: Response) => {
+  try {
+    if (req.body.password)
+      throw new Error("This route is not for updating password");
+
+    const [user] = await db
+      .update(users)
+      .set({ ...req.body })
+      .where(eq(users.id, Number(req.user?.id)))
+      .returning();
+
+    res.status(200).json({ status: "success", updatedUser: user });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ status: "error", message: "error from updateMe, check console" });
+    console.log(err);
+  }
+};
+
+// Update Password
+export const updatePassword = async (req: CustomRequest, res: Response) => {
+  try {
+    if (
+      !req.body.newPassword ||
+      !req.body.passwordConfirm ||
+      !req.body.oldPassword
+    )
+      throw new Error("Please provide all the details");
+
+    // Getting the user
+    const [currentUser] = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, Number(req.user?.id)));
+
+    if (!currentUser) throw new Error("No usere found with user ID");
+
+    // Checking the old password
+    const checkCredentials = await bcrypt.compare(
+      req.body.oldPassword,
+      currentUser.password
+    );
+
+    if (!checkCredentials) throw new Error("Invalid password, try again");
+
+    // Checking if both the new passwords match
+    if (req.body.newPassword !== req.body.passwordConfirm)
+      throw new Error("Password and Confirm Password don't match");
+
+    // Setting up the new password
+    const newPassword = await bcrypt.hash(req.body.newPassword, 8);
+
+    const [updatedUser] = await db
+      .update(users)
+      .set({ password: newPassword })
+      .where(eq(users.id, Number(req.user?.id)))
+      .returning();
+
+    res.status(200).json({ status: "success", updatedUser });
+  } catch (err) {
+    res.status(500).json({
+      status: "error",
+      message: "error from updatePassword, check console",
+    });
     console.log(err);
   }
 };
--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -5,14 +5,21 @@ import {
   protect,
   signup,
 } from "../controllers/auth.controller";
-import { getUser } from "../controllers/user.controller";
+import {
+  getUser,
+  updateMe,
+  updatePassword,
+} from "../controllers/user.controller";
 
 const router = express.Router();
 
 router.route("/sign-up").post(signup);
 router.route("/login").post(login);
 
-router.route("/delete-me").delete(protect, deleteAccount);
-router.route("/me").get(protect, getUser);
+router.use(protect);
+router.route("/me").get(getUser);
+router.route("/update-me").patch(updateMe);
+router.route("/update-password").patch(updatePassword);
+router.route("/delete-me").delete(deleteAccount);
 
 export default router;
