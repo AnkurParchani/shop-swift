e137bf8331bc62ed78d2a4b4745102e62699d1d9
Ankur Parchani <ankurparchani@gmail.com>
2024-01-13 12:33:21 +0530

    frontend-add: review container in seperate item page

--- frontend/src/app/items/[itemId]/ReviewContainer.tsx
+++ frontend/src/app/items/[itemId]/ReviewContainer.tsx
@@ -1,6 +1,15 @@
-import ReviewStars from "@/app/components/others/ReviewStars";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Card, CardBody, CardHeader, Divider, Image } from "@nextui-org/react";
+
+import ReviewStars from "@/app/components/others/ReviewStars";
+
 import { User } from "../../../../global";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
 
 type ReviewType = {
   content: string;
@@ -13,38 +22,55 @@ type ReviewType = {
 
 const ReviewContainer = ({ reviews }: { reviews: ReviewType[] }) => {
   return (
-    <div>
-      {reviews.map((review) => {
-        const { id, content, isEdited, stars, date, user } = review;
-        const { email: userEmail, name: userName, image } = user;
+    <div className="px-1 py-5">
+      <h2 className="mb-2 text-lg font-semibold text-white">Reviews</h2>
+      <Swiper
+        slidesPerView={1}
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{ clickable: true }}
+        speed={3000}
+        modules={[Pagination]}
+      >
+        {reviews.map((review) => {
+          const { id, content, isEdited, stars, date, user } = review;
+          const { email: userEmail, name: userName, image } = user;
 
-        return (
-          <Card key={id}>
-            <CardHeader className="flex items-start gap-3">
-              <Image
-                alt={`${userName} image`}
-                height={40}
-                className="rounded-full"
-                radius="sm"
-                src={user?.image?.path || "/images/default-user.jpg"}
-                width={40}
-              />
-              <div className="flex flex-col ">
-                <p className="text-md capitalize text-gray-200">{userName}</p>
-                <p className="text-xs text-gray-500">{userEmail}</p>
-                <p className="mt-2 text-xs text-default-500">
-                  <ReviewStars star={+stars} />
-                </p>
-              </div>
-            </CardHeader>
-            <Divider />
+          return (
+            <SwiperSlide key={id} className="cursor-pointer">
+              <Card className=" border-3 border-gray-600 bg-transparent pb-8">
+                <CardHeader className="flex items-start gap-3">
+                  <Image
+                    alt={`${userName} image`}
+                    height={40}
+                    className="rounded-full"
+                    radius="sm"
+                    src={user?.image?.path || "/images/default-user.jpg"}
+                    width={40}
+                  />
+                  <div className="flex flex-col ">
+                    <p className="text-md capitalize text-gray-200">
+                      {userName}
+                    </p>
+                    <p className="text-xs text-gray-500">{userEmail}</p>
+                    <p className="mt-2 text-xs text-default-500">
+                      <ReviewStars star={+stars} />
+                    </p>
+                  </div>
+                </CardHeader>
+                <Divider />
 
-            <CardBody className="text-sm text-gray-300">
-              <p className="">{content}</p>
-            </CardBody>
-          </Card>
-        );
-      })}
+                <CardBody className="text-sm text-gray-300">
+                  <p className="">{content}</p>
+                </CardBody>
+              </Card>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
